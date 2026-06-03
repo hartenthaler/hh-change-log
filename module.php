afe3624d4fe0dfdf19ee9f879dd3bb2c73fb14b4
Hermann Hartenthaler <hermann@hartenthaler.de>
2026-06-03 11:09:56 +0200

    Use core translations outside module catalog

--- module.php
+++ module.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Hartenthaler\Webtrees\ChangeLog;
 
+require __DIR__ . '/src/Internationalization/MoreI18N.php';
 require __DIR__ . '/ChangeLogTabModule.php';
 
 return new ChangeLogTabModule();
